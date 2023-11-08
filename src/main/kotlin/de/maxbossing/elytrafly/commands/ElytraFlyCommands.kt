772467ebab9edc3f6cfebd356523a7f99569c8e4
Max Bossing <info@maxbossing.de>
2023-11-08 20:01:10 +0100

    feat: Add command to save the config

--- src/main/kotlin/de/maxbossing/elytrafly/commands/ElytraFlyCommands.kt
+++ src/main/kotlin/de/maxbossing/elytrafly/commands/ElytraFlyCommands.kt
@@ -1,8 +1,15 @@
 package de.maxbossing.elytrafly.commands
 
+import de.maxbossing.elytrafly.cBase
 import de.maxbossing.elytrafly.gui.ZoneGUI
 import de.maxbossing.elytrafly.data.Permissions
+import de.maxbossing.elytrafly.data.saveConfig
+import de.maxbossing.mxpaper.extensions.bukkit.cmp
+import de.maxbossing.mxpaper.extensions.bukkit.plus
+import de.maxbossing.mxpaper.main.prefix
+import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.literalArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 
 object ElytraFlyCommands {
@@ -10,6 +17,12 @@ object ElytraFlyCommands {
         withPermission(Permissions.GUI)
         playerExecutor { player, _ ->
             ZoneGUI(player)
+        }
+        literalArgument("save") {
+            anyExecutor {sender, _ ->
+                saveConfig()
+                sender.sendMessage(prefix + cmp("Config Saved", cBase))
+            }
         }
     }
 }
